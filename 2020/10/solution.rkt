8c0db65debbb25c114825a1928b6bfd3628503ef
Max S. New <maxsnew@gmail.com>
2020-12-10 08:27:20 -0500

    10 part a

--- 2020/10/solution.rkt
+++ 2020/10/solution.rkt
@@ -5,6 +5,8 @@
          fiddle/stdlib/IO
          "../../regex.rkt")
 
+
+;; Parsing
 (define! space/lex
   (! exact-string/lex "\n" 'space))
 (define sig (list space/lex number/lex))
@@ -17,3 +19,22 @@
 
 (def-thunk (! parse-file f)
   (! parse-colist (~! read-all-chars f)))
+
+(def-thunk (! insert-diff (list 1diffs 2diffs 3diffs) (list lo hi))
+  (patc (! - hi lo)
+    [1 [1diffs <- (! + 1 1diffs)]
+     (ret (list 1diffs 2diffs 3diffs))]
+    [2 [2diffs <- (! + 1 2diffs)]
+     (ret (list 1diffs 2diffs 3diffs))]
+    [3 [3diffs <- (! + 1 3diffs)]
+     (ret (list 1diffs 2diffs 3diffs))]))
+
+(def-thunk (! main-a f)
+  [inp <- (! <<v swap sort < 'o list<-colist (~! parse-file f))]
+  (patc (! CBN (~! cl-cons 0 (~! colist<-list inp))
+           % n> windows2
+           % n> (~! cl-foldl^ insert-diff (list 0 0 1))
+           % n$)
+        [(list 1diffs _ 3diffs)
+         (! * 1diffs 3diffs)]))
+
