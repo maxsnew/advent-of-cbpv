cb8b9e87b76b9506f017a9ce921da2e1c0273000
Max S. New <maxsnew@gmail.com>
2020-12-12 07:11:44 -0500

    day 12 part a

---
+++ 2020/12/main.rkt
@@ -0,0 +1,11 @@
+#lang fiddle
+(require fiddle/prelude
+         fiddle/stdlib/IO
+         "solution.rkt")
+
+
+
+(! apply/cmd-line
+   (~ (copat [("a" f #:bind) (! idiom^ (~! displayall 'manhattan-distance-from-start:) (~! main-a f))]
+             [("b" f #:bind) (! idiom^ (~! displayall 'idk:) (~! main-b f))]
+             [() (! error "usage: racket main.rkt (a|b) INPUTFILE")])))
--- 2020/12/solution.rkt
+++ 2020/12/solution.rkt
@@ -22,3 +22,54 @@
      % n> (~! chunks 2)
      ;; % n> (~! cl-map debug)
      % n$))
+
+;; A Dir is 'N 'S 'E or 'W
+;; A Pt is a (List Nat Nat)
+;; A State is a (List Pt Dir)
+
+(def-thunk (! scale (list x y) s) (! idiom^ List (~! * s x) (~! * s y)))
+(def-thunk (! add-pt (list x1 y1) (list x2 y2))
+  (! idiom^ List (~! + x1 x2) (~! + y1 y2)))
+(def-thunk (! add-x pt x) (! add-pt pt (list x 0)))
+(def-thunk (! add-y pt y) (! add-pt pt (list 0 y)))
+
+(def-thunk (! rotate ang1 ang2) (! <<v swap modulo 360 'o + ang1 ang2))
+
+(def/copat (! vec<-dir)
+  [  (0) (ret (list  1  0))]
+  [ (90) (ret (list  0  1))]
+  [(180) (ret (list -1  0))]
+  [(270) (ret (list  0 -1))])
+
+(def/copat (! follow-instruction)
+  [((list loc dir) (list 'N n)) [loc <- (! add-y loc n)]
+   (ret (list loc dir))]
+  [((list loc dir) (list 'S n)) [loc <- (! <<v add-y loc 'o * -1 n)]
+   (ret (list loc dir))]
+  [((list loc dir) (list 'E n)) [loc <- (! add-x loc n)]
+   (ret (list loc dir))]
+  [((list loc dir) (list 'W n)) [loc <- (! <<v add-x loc 'o * -1 n)]
+   (ret (list loc dir))]
+  [((list loc dir) (list 'L n)) [dir <- (! rotate dir n)]
+   (ret (list loc dir))]
+  [((list loc dir) (list 'R n)) [dir <- (! <<v rotate dir 'o * -1 n)]
+   (ret (list loc dir))]
+  [((list loc dir) (list 'F n)) [loc <- (! <<v add-pt loc 'o swap scale n 'o vec<-dir dir)]
+   (ret (list loc dir))])
+
+(def-thunk (! follow-instructions is start-pt start-dir)
+  (! cl-foldl is
+     (~ (λ (st i) (do (! displayall 'update st i) (! follow-instruction st i))))
+     (list start-pt start-dir)))
+
+(def-thunk (! main-a f)
+  (patc (! follow-instructions (~! parse (~! read-all-chars f)) (list 0 0) 0)
+        [(list (list x y) dir)
+         (! displayall 'loc: (list x y))
+         (! displayall 'dir: dir)
+         (! idiom^ + (~! abs x) (~! abs y))]))
+
+(def-thunk (! main-b) (! error))
+
+(provide main-a
+         main-b)
