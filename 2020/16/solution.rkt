3ceab6978d7bf3bc2ae5d500116c0686e0b789f7
Max S. New <maxsnew@gmail.com>
2020-12-16 08:30:15 -0500

    16 part a (26s (mostly parsing))

--- 2020/16/solution.rkt
+++ 2020/16/solution.rkt
@@ -25,12 +25,44 @@
      % n> (~! sep-by '())
      % n$))
 
+(def-thunk (! between lo hi x)
+  (! and (~! <= lo x) (~! <= x hi)))
+
+(def-thunk (! p<-2ranges lo1 hi1 _ lo2 hi2)
+  (ret (~ (λ (x)
+            (! or (~! between lo1 hi1 x)
+                  (~! between lo2 hi2 x))))))
+
+(def/copat (! p<-rule)
+  [(name x)
+   (cond [(! number? x) (! p<-2ranges x)]
+         [else          (! p<-2ranges)])])
+
+(def-thunk (! violates-all? ps x)
+  (! CBN (~! colist<-list ps)
+     % n> (~! cl-map (~! <<v not 'o swap $ x))
+     % n> all?
+     % n$))
+
+(def-thunk (! violations ps nums)
+  (patc (! CBN  (~! colist<-list nums)
+           % n> (~! cl-filter (~! violates-all? ps))
+           % n> (~! idiom^ view)
+           % n$)
+        [(cons n _) (ret (list n))]
+        [x (ret x)]))
+
 (def-thunk (! main-a f)
-  (! CBN  (~! parse f)
-     % n> (~! cl-map debug)
-     % n> cl-length
-     % n$)
-  )
+  (patc (! list<-colist (~! parse f))
+    [(list rules (cons _ my-ticket) (cons _ nearby-tix))
+     (! displayall 'parsed)
+    [ps <- (! map (~! apply p<-rule) rules)]
+    (! CBN  (~! colist<-list nearby-tix)
+       % n> (~! cl-map (~! violations ps))
+       % n> (~! cl-bind^ colist<-list)
+       % n> (~! cl-map debug)
+       % n> (~! cl-foldl^ + 0)
+       % n$)]))
 
 (def-thunk (! main-b f)
   (! error 'nyi)
